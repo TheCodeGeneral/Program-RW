cd38fd6992d504e2bb4114837c6f08e5d60967c3
TheGeneralCode <camrpierce@gmail.com>
2021-09-18 00:06:18 -0500

    Update ProgramRead.cpp

--- ProgramRead/ProgramRead/ProgramRead.cpp
+++ ProgramRead/ProgramRead/ProgramRead.cpp
@@ -17,44 +17,85 @@ You are going to code a console program that will:
 
 #include <iostream>
 #include <Windows.h>
+#include <vector>
+
+uintptr_t GetVarBaseAddress(HANDLE hProc, uintptr_t addr, std::vector<unsigned int> offsets)
+{
+    uintptr_t newPtr = addr;
+
+    /*  Given address of value
+        address = Value = ?
+
+        base ptr -> address + offset4 = address
+
+        base ptr -> address + offset3 = address
+
+        base ptr -> address + offset2 = address
+
+        static base -> address + offset1 = address
+    */
+    for (unsigned int i = 0; i < offsets.size(); ++i)
+    {
+        bool status = ReadProcessMemory(hProc, (LPCVOID*)newPtr, &newPtr, sizeof(addr), NULL);
+        if (status == 0)
+        {
+            std::cout << "ReadProcessMemory call1 Failed\nError Code: " << GetLastError() << std::endl;
+            system("pause");
+            CloseHandle(hProc);
+            return -1;
+        }
+        newPtr += offsets[i];
+    }
+    return newPtr;
+}
 int main()
 {
     int procID;
     int intRead;
     uintptr_t ptr2int = 0x0;
+    uintptr_t ptr2ptr2 = 0x0;
     std::cout << "Enter the ProcID: ";
     std::cin >> procID;
+    // Open a handle to a program
     HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, false, (DWORD64)procID);
     if (hProc == NULL)
     {
         std::cout << "OpenProcess Failed\nError Code: " << GetLastError() << std::endl;
-        getchar();
-        exit(-1);
+        system("pause");
+        return -1;
     }
 
+    // Get the value of a ptr to an int
     std::cout << "Enter the Address of ptr2int: ";
     std::cin >> std::hex >> ptr2int;
-    std::cout << "ptr2int = 0x" << std::hex << ptr2int << std::endl;
-    getchar();
-    if (ReadProcessMemory(hProc, (LPCVOID*)ptr2int, &ptr2int, sizeof(uintptr_t), NULL) == 0)
+    if (ptr2int != NULL)
     {
-        std::cout << "ReadProcessMemory call1 Failed\nError Code: " << GetLastError() << std::endl;
-        getchar();
-        exit(-1);
-    }
-    if (ReadProcessMemory(hProc, (LPCVOID*)ptr2int, &intRead, sizeof(uintptr_t), NULL) == 0)
-    {
-        std::cout << "ReadProcessMemory call2 Failed\nError Code: " << std::dec << GetLastError() << std::endl;
-        getchar();
-        exit(-1);
-    }
-    else
-    {
+        std::cout << "ptr2int = 0x" << std::hex << std::uppercase << ptr2int << std::endl;
+        if (ReadProcessMemory(hProc, (LPCVOID*)ptr2int, &ptr2int, sizeof(uintptr_t), NULL) == 0 || ReadProcessMemory(hProc, (LPCVOID*)ptr2int, &intRead, sizeof(uintptr_t), NULL) == 0)
+        {
+            std::cout << "ReadProcessMemory call Failed\nError Code: " << GetLastError() << std::endl;
+            system("pause");
+            CloseHandle(hProc);
+            return -1;
+        }
+
         std::cout << "intRead = " << std::dec << intRead << std::endl;
-        std::cout << "Press ENTER to quit" << std::endl;
-        getchar();
     }
+
+    // Get the value of a multilevel ptr to an int
+    std::cout << "Enter address of ptr2ptr2: " << std::endl;
+    std::cin >> std::hex >> ptr2ptr2;
+    if (ptr2ptr2 != NULL)
+    {
+        std::cout << "ptr2ptr2 = 0x" << std::hex << std::uppercase << ptr2ptr2 << std::endl;
+        intRead = GetVarBaseAddress(hProc, ptr2ptr2, std::vector<unsigned int>{0, 0, 0, 0});
+        std::cout << "intRead = " << std::dec << intRead << std::endl;
+    }
+
+
     CloseHandle(hProc);
     return 0;
 }
+
+
 
